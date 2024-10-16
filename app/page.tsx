f3bba6eacdb2c0c68a804ccc4a94ad875487ac5a
TakumiHendricksDev <takumihendricksdev@gmail.com>
2024-10-16 18:47:13 -0500

    get frontend to work with api

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+const getData = async () => {
+  try {
+    const users = await fetch("http://localhost:8000/api/v1/users", {
+      cache: 'no-cache',
+    })
+    const data = await users.json();
+    return data.users;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+type User = {
+  id: number;
+  name: string;
+}
+
+const Home = async () => {
+  const data: User[] = await getData();
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center gap-5">
+      {data?.map((user) => (
+        <p className="border border-gray-100 p-2 rounded-md text-3xl" key={user.id}>
+          {user.name}
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
